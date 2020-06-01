c8b4c4b7aad702cb2ea9252e438c5a4434b6c2b8
Cary Williams <cary.l.williams00@gmail.com>
2020-06-01 17:58:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,23 +3,37 @@ def create_an_empty_array
 end
 
 def create_an_array
+<<<<<<< HEAD
   letters = ["A", "B", "C", "D"]
+=======
+letters = ["A", "B", "C", "D"]
+>>>>>>> c470919313c80dc228f901218c7f128e5fe17a95
 end
 
 
 def add_element_to_end_of_array(array, element)
+<<<<<<< HEAD
   #array = ["wow", "I", "am", "really", "learning"]
   #array << "arrays!"
   array << element 
+=======
+letters << "E"
+>>>>>>> c470919313c80dc228f901218c7f128e5fe17a95
 end
 
 
 def add_element_to_start_of_array(array, element)
+<<<<<<< HEAD
   array.unshift(element)
+=======
+ 
+ 
+>>>>>>> c470919313c80dc228f901218c7f128e5fe17a95
 end
 
 
 def remove_element_from_end_of_array(array)
+<<<<<<< HEAD
   element = array.pop
 end 
 
@@ -30,15 +44,42 @@ end
 
 def retrieve_element_from_index(array, index_number)
  array[2]  
+=======
+ 
+  
+end
+
+def remove_element_from_start_of_array(array)
+
+
+  
+end
+
+def retrieve_element_from_index(array, index_number)
+ 
+ 
+
+>>>>>>> c470919313c80dc228f901218c7f128e5fe17a95
 end
 
 
 def retrieve_first_element_from_array(array)
+<<<<<<< HEAD
  array[0]
 end
 
 def retrieve_last_element_from_array(array)
   array[-1]
+=======
+  
+  
+  
+end
+
+def retrieve_last_element_from_array(array)
+
+
+>>>>>>> c470919313c80dc228f901218c7f128e5fe17a95
 end
 
  
